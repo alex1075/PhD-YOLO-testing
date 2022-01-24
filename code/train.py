06c04e892dce97bd03aefd816e0c20d5490ca2db
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-01-24 16:09:19 +0000

    yolo

---
+++ code/train.py
@@ -0,0 +1,58 @@
+from tensorflow.keras import callbacks
+
+from yolov4.tf import YOLOv4, YOLODataset, SaveWeightsCallback
+
+yolo = YOLOv4()
+
+yolo.config.parse_names("data.names")
+yolo.config.parse_cfg("yolov4-tiny.cfg")
+
+yolo.make_model()
+yolo.load_weights(
+    "./yolov4-tiny.conv.29",
+    weights_type="yolo",
+)
+yolo.summary(summary_type="yolo")
+
+for i in range(29):
+    yolo.model.get_layer(index=i).trainable = False
+
+yolo.summary()
+
+train_dataset = YOLODataset(
+    config=yolo.config,
+    dataset_list="train/labels/train/",
+    image_path_prefix="train/images/train/",
+    training=True,
+)
+
+val_dataset = YOLODataset(
+    config=yolo.config,
+    dataset_list="./data/val.txt",
+    image_path_prefix="/data/val",
+    training=False,
+)
+
+yolo.compile()
+
+_callbacks = [
+    callbacks.TerminateOnNaN(),
+    callbacks.TensorBoard(
+        log_dir="./logs",
+        update_freq=200,
+        histogram_freq=1,
+    ),
+    SaveWeightsCallback(
+        yolo=yolo,
+        dir_path="./trained",
+        weights_type="yolo",
+        step_per_save=2000,
+    ),
+]
+
+yolo.fit(
+    train_dataset,
+    callbacks=_callbacks,
+    validation_data=val_dataset,
+    verbose=3,  # 3: print step info
+)
