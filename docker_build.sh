4b1a44995e8e46ac5ce2e513d160ec41afc1ae48
Alexander Hunt <alexander.hunt@ed.ac.uk>
2022-01-30 10:28:40 +0000

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   docker_build.sh

--- docker_build.sh
+++ docker_build.sh
@@ -5,12 +5,12 @@ PS3='Select building os architechture: '
 option=("arm64" "amd64" "Quit")
 select fav in "${option[@]}"; do
     case $fav in
-        "arm64")
+        "amd64")
             echo "Building dockerfile for arm64"
             docker build . -t alex1075/phd:latest -f Dockerfiles/Tensorflow/Dockerfile && docker build . -t alex1075/phd:latest-gpu -f Dockerfiles/Tensorflow-GPU/Dockerfile 
             docker push alex1075/phd:latest && docker push alex1075/phd:latest-gpu
             ;;
-        "amd64")
+        "arm64")
             echo "Building dockerfile for amd64"
             docker build . -t alex1075/phd:latest-arm -f Dockerfiles/Tensorflow-arm/Dockerfile
             docker push alex1075/phd:latest-arm
